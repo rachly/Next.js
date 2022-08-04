0013d0267f9cbe2c9d88523d47d0ebf609b69c91
rachly <rr0583237016@gmail.com>
2022-08-04 20:50:46 +0300

    blogs commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (<div>
+  return (
 
     <div>
-      Home Page
+    <h2 className={styles.container}> Home Page !</h2> 
+    <Image src="/images/home.jpg"alt='home' width="800"height="533"/>
     </div>
-  </div>
+  
   );
 };
 
